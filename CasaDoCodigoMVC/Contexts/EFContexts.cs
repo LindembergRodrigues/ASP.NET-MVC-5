38a7a4c435d81dc80e164b32097d6a202f470b3b
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-05-17 16:39:58 -0300

    Conexão com o entity

---
+++ CasaDoCodigoMVC/Contexts/EFContexts.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+namespace CasaDoCodigoMVC.Contexts
+{
+    public class EFContexts : DbContext 
+    {
+        public EFContexts() : base("Asp_Net_MVC_CS") { }
+
+    }
+}
