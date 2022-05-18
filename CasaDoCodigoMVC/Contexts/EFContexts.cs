afd66d9215aa7d6c3942fd0632b1e437a98f1880
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-05-18 00:06:42 -0300

    context

--- CasaDoCodigoMVC/Contexts/EFContexts.cs
+++ CasaDoCodigoMVC/Contexts/EFContexts.cs
@@ -1,10 +1,14 @@
-﻿using System.Data.Entity;
+﻿using CasaDoCodigoMVC.Models;
+using System.Data.Entity;
 
 namespace CasaDoCodigoMVC.Contexts
 {
     public class EFContexts : DbContext 
     {
         public EFContexts() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
 
     }
 }
