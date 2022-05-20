fac55ecd9e945387becee4836a7a0d48a4c70937
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-05-20 17:05:09 -0300

    Testando VIEW

--- CasaDoCodigoMVC/Controllers/FabricanteController.cs
+++ CasaDoCodigoMVC/Controllers/FabricanteController.cs
@@ -1,5 +1,8 @@
 ﻿using CasaDoCodigoMVC.Data;
+using CasaDoCodigoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using System.Net;
 
 namespace CasaDoCodigoMVC.Controllers
 {
@@ -8,7 +11,58 @@ namespace CasaDoCodigoMVC.Controllers
         private EFContext context = new EFContext();
         public IActionResult Index()
         {
-            return View(context.Fabricante.OrderBy(c => c.Nome));
+            return View(context.Fabricante.OrderBy(c => "-" + c.Nome + "-"));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricante.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(fabricante);
+        }
+        public IActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+            Fabricante fabricante = context.Fabricante.Find(id);
+            if (fabricante == null)
+            {
+                return NotFound(); //HttpNotFound(); 
+            }
+
+            return View(fabricante);
+        }
+
+        public IActionResult Delete(long id)
+        {
+            Fabricante fabricante = context.Fabricante.Find(id);
+            context.Fabricante.Remove(fabricante);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+
         }
     }
 }
