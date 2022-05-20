82cb63bef1e78587940eb2c3438da997fdd342e7
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-05-20 11:28:02 -0300

    ajuste

---
+++ CasaDoCodigoMVC/Controllers/FabricanteController.cs
@@ -0,0 +1,14 @@
+﻿using CasaDoCodigoMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigoMVC.Controllers
+{
+    public class FabricanteController : Controller
+    {
+        private EFContext context = new EFContext();
+        public IActionResult Index()
+        {
+            return View(context.Fabricante.OrderBy(c => c.Nome));
+        }
+    }
+}
