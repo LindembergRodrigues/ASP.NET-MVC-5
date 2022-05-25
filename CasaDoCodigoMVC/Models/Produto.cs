a6b3b7a33a29e8e11953a9225f56ec937c452ace
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2022-05-25 16:07:26 -0300

    novo plano de negócio

---
+++ CasaDoCodigoMVC/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace CasaDoCodigoMVC.Models
+{
+    public class Produto
+    {
+
+        public int PrudutoID { get; set; }
+        public String  Nome { get; set; }
+
+        public int CategoriaId { get; set; }
+        public int FabricanteId { get; set; }
+        public Categoria categoria { get; set; }
+        public Fabricante fabricante { get; set; }
+    }
+}
